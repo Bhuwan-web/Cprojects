ad5dbb9c6856c20d9e5124a6d00f7b0b22428222
Bhuwan-web <67514385+Bhuwan-web@users.noreply.github.com>
2021-06-21 20:22:31 +0545

    isPrime program made and tested well

--- classProjects.c
+++ classProjects.c
@@ -1,4 +1,23 @@
 #include<stdio.h>
+int isPrime();
 int main(){
     //all classroom projectsn will be combined in here
+    isPrime();
+}
+int isPrime(){
+    int num;
+    printf("\n\n Program to check if it's prime or not \n\
+    \tEnter a number: ");
+    scanf("%d",&num);
+    int i=2;
+    while(i<num){
+        if(num%i==0){
+            printf("%d is not a Prime Number",num);
+            break;
+        }
+        i++;
+    }
+    if(i==num){
+        printf("%d is prime number",num);
+    }
 }
