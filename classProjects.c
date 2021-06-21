43f2bb02f6ad9e30299077fdd28b2369da04265b
Bhuwan-web <67514385+Bhuwan-web@users.noreply.github.com>
2021-06-21 22:42:51 +0545

    Even number program added

--- classProjects.c
+++ classProjects.c
@@ -1,4 +1,18 @@
 #include<stdio.h>
+void evenNumberLoop();
 int main(){
     //all classroom projectsn will be combined in here
+    evenNumberLoop();
+}
+void evenNumberLoop(){
+    int num;
+    printf("Print Even number upto: ");
+    scanf("%d",&num);
+    int i=0;
+    while(i<=num){
+        if(i%2==0){
+            printf("%d\n",i);
+        }
+        i++;
+    }
 }
